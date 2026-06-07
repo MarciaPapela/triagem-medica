d04e809c74b15a183500dbca2f856e6a8ff6eeec
Marcia Papela <marciapapela@gmail.com>
2026-06-07 16:20:29 +0200

    chore: create Spring Boot project with initial dependencies

---
+++ src/main/java/com/grupoiv/triagemmedica/TriagemMedicaApplication.java
@@ -0,0 +1,13 @@
+package com.grupoiv.triagemmedica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriagemMedicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriagemMedicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupoiv/triagemmedica/TriagemMedicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupoiv.triagemmedica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriagemMedicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
