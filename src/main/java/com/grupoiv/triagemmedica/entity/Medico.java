3d761ba53f8131220e104b756b3ad30db102e7ae
Marcia Papela <marciapapela@gmail.com>
2026-06-07 17:01:57 +0200

    feat: add doctor entity

---
+++ src/main/java/com/grupoiv/triagemmedica/entity/Medico.java
@@ -0,0 +1,64 @@
+package com.grupoiv.triagemmedica.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "medicos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Medico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String apelido;
+
+    @Column(nullable = false)
+    private String especialidade;
+
+    @Column(nullable = false, unique = true)
+    private String numeroOrdem;
+
+    @Column(nullable = false)
+    private String celular;
+
+    @Builder.Default
+    private Boolean disponibilidade = true;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        LocalDateTime now = LocalDateTime.now();
+        createdAt = now;
+        updatedAt = now;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
